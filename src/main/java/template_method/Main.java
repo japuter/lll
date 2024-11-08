895b2be737cb8986a8cb4aee890d65da9dbe146e
Jasper Kaira <112175831+japuter@users.noreply.github.com>
2024-11-08 12:16:04 +0200

    uop

---
+++ src/main/java/template_method/Main.java
@@ -0,0 +1,8 @@
+package template_method;
+
+public class Main {
+    public static void main(String[] args) {
+        Game diceRollRace = new DiceRollRace();
+        diceRollRace.play(5);
+    }
+}
